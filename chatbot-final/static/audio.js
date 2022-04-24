bcf68b3351cc12e40ff06295de68130f45204b2f
Raghav3195 <guptaraghav2006@gmail.com>
2022-04-24 15:34:04 +0530

    firse purana update chipka diya hai

--- chatbot-final/static/audio.js
+++
@@ -1,9 +0,0 @@
-let speech = new SpeechSynthesisUtterance();
-
-speech.lang = "en-US";
-speech.text = msg;
-speech.volume = 1;
-speech.rate = 1;
-speech.pitch = 1;                
-
-window.speechSynthesis.speak(speech);
